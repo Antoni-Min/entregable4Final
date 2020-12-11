635d0b63a25a5f8b7455d72a543ccb5995c0d3e6
Antoni-Min <71768223+Antoni-Min@users.noreply.github.com>
2020-12-11 20:19:35 +0100

    Add files via upload

---
+++ Entregable4Final/entregable4-backend-books/src/controllers/books.controller.ts
@@ -0,0 +1,64 @@
+import { Request, Response } from "express";
+
+import BooksModel from "../models/books.model";
+class BooksController {
+    public async getBooks(req: Request, res: Response){
+        try {
+
+
+            
+            const allBooks = await BooksModel.find();
+            res.send(allBooks);
+
+        } catch (error) {
+            console.log(error);
+            res.sendStatus(404);            
+        }
+    };
+
+    public async postBooks(req: Request, res: Response){
+        try {
+            const bookTitle = req.body.title;
+            const newBook = new BooksModel();
+            newBook.title = bookTitle;
+
+            await newBook.save();
+            res.send(`¡Se ha añadido ${bookTitle} a la Biblioteca!`);
+
+        } catch (error) {
+            console.log(error);
+            res.sendStatus(404);            
+        }
+    };
+
+    public async deleteBooks(req: Request, res: Response){
+        try {
+
+            const bookToDelete = req.params.id;
+            const response = await BooksModel.deleteOne({_id: bookToDelete});
+
+            res.send(`El libro con identificador ${bookToDelete} ha sido borrado de la Biblioteca`);
+
+        } catch (error) {
+            console.log(error);
+            res.sendStatus(404);
+
+        }
+    };
+
+    public async updateBooks(req: Request, res: Response){
+        try {
+            const bookToUpdate = req.body;
+            const response = await BooksModel.updateOne({_id: bookToUpdate._id}, {title: bookToUpdate.title});
+                                                        
+
+            res.send(`El libro con identificador ${bookToUpdate._id} ha sido editado.`);
+
+        } catch (error) {
+            console.log(error);
+            res.sendStatus(404);
+        }
+    };
+}
+
+export const booksController = new BooksController();
